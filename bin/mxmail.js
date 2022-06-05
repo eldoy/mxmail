1536fbfece25f3a408f688ebb19ddb2ba6dd22ce
Vidar Eldøy <vidar@eldoy.com>
2022-06-05 11:10:09 +0200

    Formatting

--- bin/mxmail.js
+++ bin/mxmail.js
@@ -2,6 +2,7 @@
 const fs = require('fs')
 const path = require('path')
 const os = require('os')
+
 const settings = path.join(os.homedir(), '.mxmail.json')
 if (!fs.existsSync(settings)) {
   console.log(`Settings file not found in ${settings}`)
@@ -13,12 +14,15 @@ const mxmail = require('../index.js')
 let { from, config, aliases = {} } = require(settings)
 let to = process.argv[2] || ''
 to = aliases[to] || to
+
 const subject = process.argv[3]
 const text = process.argv[4] || ''
 const { html } = formatter(text)
+
 const attachments = process.argv.slice(5).map(function(file) {
   const filename = path.basename(file)
   return { filename, path: file }
 })
+
 const mailer = mxmail(config)
 mailer({ to, from, subject, text, html, attachments })
